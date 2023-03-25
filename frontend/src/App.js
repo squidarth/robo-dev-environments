c7c950311f08e3130c45da88aece2e11d3660d95
Sid Shanker <sid.shanker@baseten.co>
2023-03-25 15:50:03 -0700

    add supabase info.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,4 +15,38 @@ function App() {
   );
 }
 
+const supabase = createClient('https://ymgwygmadbpzlefijsnm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltZ3d5Z21hZGJwemxlZmlqc25tIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk3ODE0MjMsImV4cCI6MTk5NTM1NzQyM30.EyODb2ILaFfWg5UnxHFM6GpZtQHcUqoTi8SpRuz-6nM')
+
+export function SignInWithGithubButton() {
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        // Handle session updates
+        console.log(event, session)
+      }
+    )
+
+    // Cleanup auth listener
+    return () => {
+      authListener.unsubscribe()
+    }
+  }, [])
+
+  const signInWithGithub = async () => {
+    const { user, error } = await supabase.auth.signIn({
+      provider: 'github'
+    })
+
+    if (error) {
+      console.log('Error signing in with GitHub:', error.message)
+    }
+  }
+
+  return (
+    <button onClick={signInWithGithub}>
+      Sign in with GitHub
+    </button>
+  )
+}
+
 export default App;
