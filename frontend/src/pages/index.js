64665b2a7b35904ca42131918aebb69881a3fad7
Sid Shanker <sid.shanker@baseten.co>
2023-03-25 15:35:37 -0700

    Remove google font.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,11 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { createClient } from '@supabase/supabase-js'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 // const supabase = createClient('YOUR_SUPABASE_URL', 'YOUR_SUPABASE_PUBLIC_KEY')
 
