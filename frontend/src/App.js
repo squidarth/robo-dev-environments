b5c376f707ff55bbfb226c96c3d5ed3bd03ff2d6
Sid Shanker <sid.shanker@baseten.co>
2023-03-25 15:47:52 -0700

    Change to CRA.

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Auto dev environments
+          
+        </h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
