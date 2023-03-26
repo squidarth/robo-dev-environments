aa297b15685df9183d2617796f88a49ca438012b
Hanqi Sun <hanqi.sun@gmail.com>
2023-03-25 18:00:31 -0700

    Authenticate properly in tasks.py instead

--- tasks.py
+++ tasks.py
@@ -2,9 +2,12 @@ import os
 from celery import Celery
 from celery.utils.log import get_task_logger
 
+from gpt4 import authenticate_openai
+
 app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
 logger = get_task_logger(__name__)
 
+authenticate_openai(os.environ['OPENAI_API_KEY'])
 
 @app.task
 def create_development_environment(github_repo_url, github_access_token, user_email):
